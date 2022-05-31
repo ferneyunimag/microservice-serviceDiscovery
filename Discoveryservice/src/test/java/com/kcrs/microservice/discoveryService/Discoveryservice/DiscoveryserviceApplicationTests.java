005d963a58bcdef60d31f32cc62edc146cd67f23
ferneyunimag <ferneyunimag@gmail.com>
2022-05-31 18:48:55 -0500

    agregado el discovery service

---
+++ Discoveryservice/src/test/java/com/kcrs/microservice/discoveryService/Discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcrs.microservice.discoveryService.Discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
