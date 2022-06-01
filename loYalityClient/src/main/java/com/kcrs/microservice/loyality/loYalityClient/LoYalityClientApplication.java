35baca91006589b16c6a9a24ac11795df82ff7bd
ferneyunimag <ferneyunimag@gmail.com>
2022-05-31 22:46:28 -0500

    agregando microservicio fidelizacion

---
+++ loYalityClient/src/main/java/com/kcrs/microservice/loyality/loYalityClient/LoYalityClientApplication.java
@@ -0,0 +1,13 @@
+package com.kcrs.microservice.loyality.loYalityClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoYalityClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoYalityClientApplication.class, args);
+	}
+
+}
---
+++ loYalityClient/src/test/java/com/kcrs/microservice/loyality/loYalityClient/LoYalityClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcrs.microservice.loyality.loYalityClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoYalityClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
