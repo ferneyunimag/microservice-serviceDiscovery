ac5e8338b81700536d786e1c2438838b52c3d09b
ferneyunimag <ferneyunimag@gmail.com>
2022-05-31 23:54:42 -0500

    configurado completamente el discovery service

--- loYalityClient/src/main/java/com/kcrs/microservice/loyality/loYalityClient/LoYalityClientApplication.java
+++ loYalityClient/src/main/java/com/kcrs/microservice/loyality/loYalityClient/LoYalityClientApplication.java
@@ -2,8 +2,11 @@ package com.kcrs.microservice.loyality.loYalityClient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class LoYalityClientApplication {
 
 	public static void main(String[] args) {
