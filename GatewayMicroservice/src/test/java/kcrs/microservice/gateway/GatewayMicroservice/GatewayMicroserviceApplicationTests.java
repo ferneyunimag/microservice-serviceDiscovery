f23b94334c71d666c3215db6c7ca5ac4b8f1e603
ferneyunimag <ferneyunimag@gmail.com>
2022-06-08 17:20:32 -0500

    added gateway microservice

---
+++ GatewayMicroservice/src/test/java/kcrs/microservice/gateway/GatewayMicroservice/GatewayMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package kcrs.microservice.gateway.GatewayMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
